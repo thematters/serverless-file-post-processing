f3e354b74d1d23b28581ffa7511fc25d1fa49a40
RoberTu <robertu0717@gmail.com>
2020-06-09 22:24:29 +0800

    log on object created

--- src/imageCreated.ts
+++ src/imageCreated.ts
@@ -1,12 +1,19 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { S3Handler } from 'aws-lambda'
+import 'source-map-support/register'
 
-export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+export const handler: S3Handler = async (event, context) => {
+  // Fail on mising data
+  if (event.Records === null) {
+    context.fail('Error: Event has no records.')
+    return
+  }
+
+  event.Records.forEach((record) => {
+    // record.s3.bucket.name
+    const filename = record.s3.object.key
+    const filesize = record.s3.object.size
+    console.log(
+      `New image object has been created: ${filename} (${filesize} bytes)`
+    )
+  })
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+import { imageSize } from 'image-size'
+import imagemin from 'imagemin'
+import imageminGifsicle from 'imagemin-gifsicle'
+import imageminJpegtran from 'imagemin-jpegtran'
+import imageminPngquant from 'imagemin-pngquant'
+import imageminSvgo from 'imagemin-svgo'
+import mime from 'mime-types'
+import sharp from 'sharp'
+
